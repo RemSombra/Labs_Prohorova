670a08fc06c7b529c663f085e17fff0e9d00b6b1
RemSombra <96395885+RemSombra@users.noreply.github.com>
2022-12-21 05:51:34 +0300

    Add files via upload

---
+++ 2/test.py
@@ -0,0 +1,24 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+
+def test() -> None:
+    driver = webdriver.Edge()
+
+    driver.get("https://qa.neapro.site/login")
+
+    driver.find_element(
+        By.CSS_SELECTOR, ".fieldset:nth-child(1) input").send_keys("blackleprosy@gmail.com")
+    driver.find_element(
+        By.CSS_SELECTOR, ".fieldset:nth-child(2) input").send_keys("123456789")
+
+    driver.find_element(By.CSS_SELECTOR, ".btn").click()
+
+    time.sleep(10)
+
+if __name__ == "__main__":
+    print("test case")
+    test()
